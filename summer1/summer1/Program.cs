f2332c1b7d26e2df603320969d8e5a0808d32ce5
Sueda Tekin <115072432+SuedaTekin@users.noreply.github.com>
2025-07-08 10:25:35 +0300

    Add files via upload

---
+++ summer1/summer1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace summer1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int myInt = 20; 
+            double myDouble = myInt; 
+
+            Console.WriteLine(myInt);
+            Console.WriteLine(myDouble);
+
+
+
+            double DoubleValue = 4.78;
+            int intValue = (int)DoubleValue;
+            Console.WriteLine(DoubleValue);
+            Console.WriteLine(intValue);
+        }
+    }
+}
